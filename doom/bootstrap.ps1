9f5bca1335e10536c3a025228b81a22912a52132
Chris O'Neil <chris.oneil@gmail.com>
2019-11-23 17:49:02 +0000

    Add OKUPLOK Slaughter Map
    This map wouldn't run using the resolution scaled at 4k, so I turned it
    down to 1080p. It's a lot blockier, but it still looks good and it runs
    nice and smooth, so I'm going to keep it here for good performance with
    larger maps.

--- doom/bootstrap.ps1
+++ doom/bootstrap.ps1
@@ -186,6 +186,7 @@ DownloadWad -Name "PLUTONIA.WAD"
 DownloadWad -Name "TNT.WAD"
 DownloadWad -Name "HR.WAD"
 DownloadWad -Name "hr2final.wad"
+DownloadWad -Name "oku2v31.wad"
 DownloadWad -Name "pk_doom_sfx.wad" # Subtle high resolution sound effect improvements
 DownloadWad -Name "D1SPFX19.WAD" # Minor sprite fixes for DOOM 1
 DownloadWad -Name "D2SPFX19.WAD" # Minor sprite fixes for DOOM 2
